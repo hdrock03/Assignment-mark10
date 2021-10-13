21b7c6322f3d5e2ab0d1573eacc926e188eaed5b
hdrock03 <hardik.1rn16ec036@gmail.com>
2021-10-13 08:57:14 +0530

    feat added coditions

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
+const billingAmount = document.querySelector("#billing-amount");
+const cash = document.querySelector("#cash");
+const check = document.querySelector("#check");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
+check.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
-    if (billAmount.value > 0) {
+    if (billingAmount.value > 0) {
       
-      if (cashGiven.value >= billAmount.value) {
+      if (cash.value >= billingAmount.value) {
         
-        const amountToBeReturned = cashGiven.value - billAmount.value; 
+        const amountToBeReturned = cash.value - billingAmount.value; 
         calculateChange(amountToBeReturned);
       } else {
-        showMessage("Do you wanna wash plates?");
+        showMessage("wash the plates");
       }
     } else {
       showMessage("Invalid Bill Amount");
